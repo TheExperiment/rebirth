349127fd3043494bb128c334e1b6091fee77829c
narrowdesign <nick@narrowdesign.com>
2017-03-21 14:24:53 -0400

    crazy

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -28,6 +28,47 @@ $(function() {
 
   WIN.on('mousemove',mousemoveHandler);
 
+  $('.jsContact').on('mouseleave', function() {
+    $('.jsLogo').css({
+      transform: 'translate3d(0,0,0)'
+    })
+  })
+  $('.jsContact').on('mouseenter', function(){
+    blip($('.jsLogo'));
+    $('.jsLogo').eq(0).css({
+      transform: 'translateY('+ 4.1666+'vw)',
+      transition: '.4s'
+    })
+    $('.jsLogo').eq(1).css({
+      transform: 'translateY('+8.333+'vw)',
+      transition: '.4s'
+    })
+    $('.jsLogo').eq(2).css({
+      transform: 'translateY('+-8.333+'vw)',
+      transition: '.4s'
+    })
+    $('.jsLogo').eq(3).css({
+      transform: 'translateY('+-16.666+'vw)',
+      transition: '.4s'
+    })
+    $('.jsLogo').eq(4).css({
+      transform: 'translateX('+ 4.1666+'vw)',
+      transition: '.4s'
+    })
+    $('.jsLogo').eq(5).css({
+      transform: 'translateY('+ -4.1666+'vw)',
+      transition: '.4s'
+    })
+    $('.jsLogo').eq(6).css({
+      transform: 'translateY('+ 4.1666+'vw)',
+      transition: '.4s'
+    })
+    $('.jsLogo').eq(7).css({
+      transform: 'translateX('+ 8.333+'vw)',
+      transition: '.4s'
+    })
+  })
+
   setInterval(function(){
     $('.grit').css({
       'background-position-y': Math.floor(Math.random()*100),
@@ -47,47 +88,47 @@ $(function() {
     var color = Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)
     logo.css({
       // filter: 'blur('+blur+'px)',
-      transition: Math.ceil(Math.random() * 2000) + 1000 + 'ms',
+      transitionDuration: Math.ceil(Math.random() * 2000) + 1000 + 'ms',
       backgroundColor: 'rgb('+color+')',
       color: 'rgba('+color+',.5)',
     })
     $('.jsExpLogo').css({
       // filter: 'blur('+blur/3+'px)',
-      transition: '.5s',
+      transitionDuration: '.5s',
       fill: 'rgb('+color+')'
     })
 
     $('.jsLine').css({
       color: 'rgb('+color+')',
-      transition: '.5s',
+      transitionDuration: '.5s',
     })
     $('.jsCaret').css({
       fill: 'rgb('+color+')',
-      transition: '.5s',
+      transitionDuration: '.5s',
     })
     $('path', logo).each(function(){
       $(this).css({
       fill: 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')',
-      transition: '.5s',
-      transition: '.8s'
+      transitionDuration: '.5s',
+      transitionDuration: '.8s'
     })})
     $('circle', logo).each(function(){
       $(this).css({
       fill: 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')',
-      transition: '.8s',
-      transition: '.8s'
+      transitionDuration: '.8s',
+      transitionDuration: '.8s'
     })})
     $('rect', logo).each(function(){
       $(this).css({
       fill: 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')',
-      transition: '.4s',
-      transition: '.8s'
+      transitionDuration: '.4s',
+      transitionDuration: '.8s'
     })})
     $('polygon', logo).each(function(){
       $(this).css({
       fill: 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')',
-      transition: '1s',
-      transition: '.8s'
+      transitionDuration: '1s',
+      transitionDuration: '.8s'
     })})
     setTimeout(function(){
       $('.jsExpLogo').css({
@@ -96,24 +137,24 @@ $(function() {
       })
       logo.css({
         // 'filter': 'blur(0)',
-        transition: '2s',
+        transitionDuration: '2s',
         backgroundColor: '#352F2F',
       })
       $('path', logo).css({
         fill: 'inherit',
-        transition: '1s'
+        transitionDuration: '1s'
       })
       $('circle', logo).css({
         fill: 'inherit',
-        transition: '1.2s'
+        transitionDuration: '1.2s'
       })
       $('rect', logo).css({
         fill: 'inherit',
-        transition: '1.4s'
+        transitionDuration: '1.4s'
       })
       $('polygon', logo).css({
         fill: 'inherit',
-        transition: '.6s'
+        transitionDuration: '.6s'
       })
     },2000);
   }
@@ -132,18 +173,46 @@ $(function() {
     var color = 'rgb('+Number(Math.floor(Math.random()*155)+100)+','+Number(Math.floor(Math.random()*155)+100)+','+Number(Math.floor(Math.random()*155)+100)+')'
     BODY.css({
       background: color,
-      transition: '.6s'
+      transitionDuration: '.6s'
     })
     $('.jsContact').css({
       background: color,
-      transition: '.6s'
+      transitionDuration: '.6s'
     })
   }
 
   function mousemoveHandler(e) {
-    // if (BODY.hasClass('is-images')) {
-    //   $('.imageNav').each(function () {
-    //    this.style.setProperty( 'top', e.pageY - scrollTop, 'important' );
+    // if (e.pageY < _winH/2) {
+    //   $('.jsLogo').eq(1).css({
+    //     transform: 'translateY('+8.333+'vw)'
+    //   })
+    //   $('.jsLogo').eq(2).css({
+    //     transform: 'translateY('+-8.333+'vw)'
+    //   })
+    //   $('.jsLogo').eq(3).css({
+    //     transform: 'translateY('+-16.666+'vw)'
+    //   })
+    //   $('.jsLogo').eq(5).css({
+    //     transform: 'translateY('+8.333+'vw)'
+    //   })
+    //   $('.jsLogo').eq(7).css({
+    //     transform: 'translateX('+8.333+'vw)'
+    //   })
+    // } else {
+    //   $('.jsLogo').eq(1).css({
+    //     transform: 'translateY(0)'
+    //   })
+    //   $('.jsLogo').eq(2).css({
+    //     transform: 'translateY(0)'
+    //   })
+    //   $('.jsLogo').eq(3).css({
+    //     transform: 'translateY(0)'
+    //   })
+    //   $('.jsLogo').eq(5).css({
+    //     transform: 'translateY(0)'
+    //   })
+    //   $('.jsLogo').eq(7).css({
+    //     transform: 'translateX(0)'
     //   })
     // }
   }
